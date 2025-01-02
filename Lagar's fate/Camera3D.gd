6d5a4353f28d556bf6ebedd6c64fc8b811e94296
I'm just K <142046371+Kcodesufal@users.noreply.github.com>
2025-01-01 22:06:04 -0300

    Update Camera3D.gd

--- Lagar's fate/Camera3D.gd
+++ Lagar's fate/Camera3D.gd
@@ -17,7 +17,7 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	if parent.is_on_floor():
+	if parent.is_on_floor() or parent.is_on_wall():
 		if (floor == false and timer.get_time_left() == 0 and (rotation.x > 1.5 or rotation.x < -1.5)): 
 			timer.start() #usado para correção de ângulo de câmera pós-pulos
 		elif timer.get_time_left() > 0:
@@ -28,7 +28,18 @@ func _process(delta):
 			rotation.x = clamp (rotation.x, -1.5, 1.5)
 	else:
 		floor = false
-		pass
+''' aviso: realizar correção de bugs em todas as linhas marcadas aqui
+	#print("z:", rotation.z, " x", rotation.x, " y", parent.rotation.y)
+	if parent.is_on_wall_only():
+	#	parent.rotation.y = clamp (parent.rotation.y, -1.0, 1.0) -> objetivo: restringir rotação horizontal while on wall
+		print(parent.rotation.z)
+		if parent.global_position.x < global_position.x: # tenta realizar a alteração do ângulo da câmera, a ideia é parecer que o personagem se apoiou nas paredes (REQUER CORREÇÃO)
+			parent.rotation.z = lerp(parent.rotation.z, 0.2, 0.1)
+		else:
+			parent.rotation.z = lerp(parent.rotation.z, -0.2, 0.1)
+	elif parent.rotation.z != 0:                             # tenta realizar a alteração da câmera para ao voltar ao chão
+		parent.rotation.z = lerp(parent.rotation.z,0.0,0.08)
+''' 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _input(event):
 	if Input.is_action_just_pressed("menu"):
@@ -43,5 +54,6 @@ func _input(event):
 		
 		parent.rotate(Vector3.UP, -event.relative.x * 0.001)
 		rotate_object_local(Vector3.RIGHT, event.relative.y * -0.001)
+		
 		
 	pass
